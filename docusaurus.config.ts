23add6b006d3f19fc7a2b294698ca04c37278b22
José Antonio Parejo Maestre <japarejo@gmail.com>
2024-08-30 12:47:54 +0200

    fixed project name to repo name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'gii-is-DP1', // Usually your GitHub org/user name.
-  projectName: 'DP1', // Usually your repo name.
+  projectName: 'gii-is-DP1.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
